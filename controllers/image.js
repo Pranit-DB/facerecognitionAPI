b15cdeae1e5aed4ba8522737537413d00079b714
Pranit Bhegade <111244937+Pranit-DB@users.noreply.github.com>
2022-11-13 15:05:36 +0530

    Update image.js
    cleaning up code

--- controllers/image.js
+++ controllers/image.js
@@ -1,31 +1,23 @@
 import Clarifai from 'clarifai'
 
-// export default function imageHandler(req, res) {
-    // console.log('function called');
-    // return (req,res,db) => {
-export const imageHandler = (req, res, db)=>{
-    const { id } = req.params;
-        /// updated way
-    db('users').where('id','=',id)
-    .increment('entries',1)
-    .then(
-        ()=>{
-        db.select('*').from('users').where({id})
-            .then(user=>{
-                res.json(user[0].entries)
-            })
-            .catch(err=>res.status(404).json('user not found'))
+const { id } = req.params;
+    /// updated way
+db('users').where('id','=',id)
+.increment('entries',1)
+.then(
+    ()=>{
+    db.select('*').from('users').where({id})
+        .then(user=>{
+            res.json(user[0].entries)
         })
-    .catch(err=>res.status(400).json('entries failed'))
-    }
-// }
-// module.exports ={
-//     imageHandler : imageHandler
-// }
-
-const app = new Clarifai.App({
-      apiKey : '9d0a34e1f8ba4f08b2ea3d16f2bf2c88'
+        .catch(err=>res.status(404).json('user not found'))
     })
+.catch(err=>res.status(400).json('entries failed'))
+}
+    
+const app = new Clarifai.App({
+      apiKey : 'YOUR CLARIFAI KEY'
+})
     //TODO: fetch imgurl from server
 export const imageUrlHandler = (req, res)=>{
     app.models
@@ -38,4 +30,4 @@ export const imageUrlHandler = (req, res)=>{
             res.json(response)  
         })
        .catch(err => console.log(err,' API CALL FAILED!'));
-  }
+}
